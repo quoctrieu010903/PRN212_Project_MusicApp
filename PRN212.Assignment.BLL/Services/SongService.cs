882e950276536b60ba872e8e7079d3656935e7d2
quoctrieu010903 <luongquoctrieu165@gmail.com>
2025-04-22 07:52:39 +0700

    Done CRUD Song

--- PRN212.Assignment.BLL/Services/SongService.cs
+++ PRN212.Assignment.BLL/Services/SongService.cs
@@ -13,15 +13,15 @@ namespace PRN212.Assignment.BLL.Services
         private SongListRepo _repo = new() ;
         public void CreateSong(Song song)
         {
-            _repo.CreatePlayList(song) ;
+            _repo.CreateSong(song) ;
         }
         public void UpdateSong(Song song)
         {
-            _repo.UpdatePlayList(song);
+            _repo.UpdateSong(song);
         }
         public void DeleteSong(Song song)
         {
-            _repo.DeletePlayList(song);
+            _repo.DeleteSong(song);
         }
         public List<Song> getAllSong() => _repo.getAllPlayList();
     }
